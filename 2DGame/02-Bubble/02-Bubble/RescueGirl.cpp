dfef8f9a10fa8b2da8669d294450c0ca5fffc0be
Victorsitu <victor.martinez.m@estudiantat.upc.edu>
2021-03-30 17:21:24 +0200

    RescueGirl 1/2

---
+++ 2DGame/02-Bubble/02-Bubble/RescueGirl.cpp
@@ -0,0 +1,74 @@
+#include <cmath>
+#include <iostream>
+#include <GL/glew.h>
+#include <GL/glut.h>
+#include "RescueGirl.h"
+
+enum GirlState
+{
+    RESCUED
+} states;
+
+enum EnemyAnims
+{
+    STAND
+};
+
+
+void RescueGirl::init(const glm::ivec2& tileMapPos, ShaderProgram& shaderProgram, const glm::ivec2& posInicial)
+{
+    states = RESCUED;
+    spritesheet.loadFromFile("images/Girl.png", TEXTURE_PIXEL_FORMAT_RGBA);
+    sprite = Sprite::createSprite(glm::ivec2(32, 32), glm::vec2(1.f/7.f, 1.f/2.f), &spritesheet, &shaderProgram);
+    sprite->setNumberAnimations(1);
+
+    sprite->setAnimationSpeed(STAND, 1);
+    sprite->addKeyframe(STAND, glm::vec2(7.f/7.f, 0.f/2.f));
+    sprite->addKeyframe(STAND, glm::vec2(6.f / 7.f, 0.f / 2.f));
+    sprite->addKeyframe(STAND, glm::vec2(5.f / 7.f, 0.f / 2.f));
+    sprite->addKeyframe(STAND, glm::vec2(4.f / 7.f, 0.f / 2.f));
+    sprite->addKeyframe(STAND, glm::vec2(3.f / 7.f, 1.f / 2.f));
+    sprite->addKeyframe(STAND, glm::vec2(2.f / 7.f, 1.f / 2.f));
+    sprite->addKeyframe(STAND, glm::vec2(1.f / 7.f, 1.f / 2.f));
+
+    sprite->changeAnimation(0);
+    tileMapDispl = tileMapPos;
+    sprite->setPosition(posInicial);
+}
+
+void RescueGirl::update(int deltaTime)
+{
+    /*
+    sprite->update(deltaTime);
+    posAnt = posGirl;
+    switch (states) {
+    case RESCUED: {
+        if (map->checkIfPlayer(posGirl, glm::ivec2(32, 32)))
+        {
+        }
+    } break;
+    }
+
+    sprite->setPosition(glm::vec2(float(tileMapDispl.x + posPortal.x), float(tileMapDispl.y + posPortal.y)));*/
+}
+
+void RescueGirl::render()
+{
+    sprite->render();
+}
+
+void RescueGirl::setTileMap(TileMap* tileMap)
+{
+    map = tileMap;
+}
+
+void RescueGirl::setPlayer(Player* p)
+{
+    player = p;
+}
+
+void RescueGirl::setPosition(const glm::vec2& pos)
+{
+    posGirl = pos;
+    sprite->setPosition(glm::vec2(float(tileMapDispl.x + posGirl.x), float(tileMapDispl.y + posGirl.y)));
+}
--- 2DGame/02-Bubble/02-Bubble/RescueGirl.h
+++ 2DGame/02-Bubble/02-Bubble/RescueGirl.h
@@ -1,5 +1,5 @@
-#ifndef _TELEPORT_INCLUDE
-#define _TELEPORT_INCLUDE
+#ifndef _RESCUEGIRL_INCLUDE
+#define _RESCUEGIRL_INCLUDE
 
 
 #include "Sprite.h"
@@ -12,11 +12,11 @@
 // all properties it needs to track its movement, jumping, and collisions.
 
 
-class Teleport
+class RescueGirl
 {
 
 public:
-    void init(const glm::ivec2& tileMapPos, ShaderProgram& shaderProgram, const glm::ivec2& posInicial, const glm::ivec2& posTeleport);
+    void init(const glm::ivec2& tileMapPos, ShaderProgram& shaderProgram, const glm::ivec2& posInicial);
     void update(int deltaTime);
     void render();
 
@@ -25,7 +25,7 @@ public:
     void setPosition(const glm::vec2& pos);
 
 protected:
-    glm::ivec2 tileMapDispl, posPortal, posTel, posAnt;
+    glm::ivec2 tileMapDispl, posGirl, posAnt;
     int jumpAngle, startY;
     Texture spritesheet;
     Sprite* sprite;
@@ -33,4 +33,4 @@ protected:
     Player* player;
 };
 
-#endif // _TELEPORT_INCLUDE
+#endif // _RESCUEGIRL_INCLUDE
